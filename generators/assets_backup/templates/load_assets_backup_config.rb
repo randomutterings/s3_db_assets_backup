4bd7d701da02e6e17a290f53a0a4d73fe5c79e9b
Chris Barnes <randomutterings@gmail.com>
2010-03-22 17:37:55 -0400

    corrected error msg in initializer

--- generators/assets_backup/templates/load_assets_backup_config.rb
+++ generators/assets_backup/templates/load_assets_backup_config.rb
@@ -2,5 +2,5 @@ config_file = "#{RAILS_ROOT}/config/assets_backup_config.yml"
 if FileTest.exists?(config_file)
   APP_CONFIG = YAML.load_file(config_file)
 else
-  raise "Can't find config.yml"
+  raise "Can't find assets_backup_config.yml"
 end
